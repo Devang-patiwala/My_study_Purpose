8fa8b862a54891aea6531a0969fc277a6c888d00
Devang-patiwala <139359286+Devang-patiwala@users.noreply.github.com>
2026-01-30 09:55:10 +0530

    Delete Python/3_Mar_2025 directory

--- Python/3_Mar_2025/p2.py
+++
@@ -1,5 +0,0 @@
-with open ("sample.txt","w") as file:
-    file.write('Hello, How are you?\n')
-    file.write('Very good morning\n')
-    file.write('Have a nice day to all\n')
-    file.write('Good Bye...\n')
--- Python/3_Mar_2025/p4.py
+++
@@ -1,5 +0,0 @@
-import pickle
-file1=open('demo.bin','wb')
-line='Yash Thakar'
-file1.write(line)
-file1.close()
